bd20af9b99552b1efbe3f8d41445e4a9391cbb0f
MrHan <844017104@qq.com>
2022-08-08 17:41:53 +0800

    init

---
+++ src/main/java/com/mrhan/localworkmng/LocalWorkMngApplication.java
@@ -0,0 +1,13 @@
+package com.mrhan.localworkmng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocalWorkMngApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LocalWorkMngApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mrhan/localworkmng/LocalWorkMngApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mrhan.localworkmng;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocalWorkMngApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
